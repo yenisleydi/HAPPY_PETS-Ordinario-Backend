551626f4d95a960e5cb3e1ccf46b66e4b4a48131
nayeli velasco <nayeserratvl@gmail.com>
2025-02-10 11:03:24 -0600

    se agrego es update al estado de la cita

--- HAPPY_PETS-Ordinario-Backend/src/main/java/com/example/HAPPY_PETS_Ordinario_Backend/model/Cita.java
+++ HAPPY_PETS-Ordinario-Backend/src/main/java/com/example/HAPPY_PETS_Ordinario_Backend/model/Cita.java
@@ -41,10 +41,6 @@ public class Cita {
     @JoinColumn(name = "id_servicio", referencedColumnName = "id_servicio")
     private ServicioVeterinaria servicioVeterinaria;
 
-    @ManyToOne
-    @JoinColumn(name = "id_duenio", referencedColumnName = "id_duenio")
-    private Duenio duenio;
-
     @Column(length = 20)
     private String estado; // Pendiente, Completada, Cancelada
 
